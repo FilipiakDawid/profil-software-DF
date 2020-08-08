47028d682c2bbc48db1e9f4a55e0cb6e251623ed
Dawid Filipiak <m302404@alunos.ipb.pt>
2020-08-08 15:20:19 +0200

    Add getting json file from API, and argparse commands

--- Controllers/MainController.py
+++ Controllers/MainController.py
@@ -3,18 +3,25 @@ import ReadJsonFile
 import argparse
 
 parser = argparse.ArgumentParser(description='Main Controller')
-parser.add_argument('--installfromFile', help="Insert Values to DataBase from JSON file", action='store_true')
+parser.add_argument('--insertdata', help="Insert Values to DataBase from JSON file", choices=['API', 'Local'])
 args = parser.parse_args()
 
 
-def add_json_file():
-    file = ReadJsonFile.readfile("persons.json")
-    json_model = AddJSON()
-    json_model.add_to_base_json(file)
+def add_json_file(choice):
+    if choice == 'Local':
+        file = ReadJsonFile.read_file_local("persons.json")
+    else:
+        file = ReadJsonFile.read_file_from_api("https://randomuser.me/api/?results=25")
+
+    if file is not None:
+        json_model = AddJSON()
+        json_model.add_to_base_json(file)
+        print("Successfully added to the base")
+    else:
+        print("Problem with reading the file from: " + choice)
 
 
-
-if args.installfromFile:
-    add_json_file()
+if args.insertdata:
+    add_json_file(args.insertdata)
 else:
     print(parser.print_help())
--- ReadJsonFile.py
+++ ReadJsonFile.py
@@ -1,7 +1,21 @@
 import json
+import requests
 
 
-def readfile(file):
-    with open(file, encoding="utf8") as f:
-        d = json.load(f)
-    return d
+def read_file_local(file):
+    try:
+        with open(file, encoding="utf8") as f:
+            d = json.load(f)
+        return d
+    except BaseException as err:
+        print("Something goes wrong: " + str(err))
+        return None
+
+
+def read_file_from_api(url):
+    response = requests.request("GET", url)
+    if response.status_code != 200:
+        print("Something goes wrong, please try again response code(" + str(response.status_code) + ")")
+        return None
+    else:
+        return response.json()
